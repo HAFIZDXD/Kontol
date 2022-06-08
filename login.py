83a9e707831d10f9f907b4a64f71c5e50371603c
HafizdXD <101934492+HAFIZDXD@users.noreply.github.com>
2022-06-08 17:06:09 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,5 @@
 import os
 
 cookie=input('MASUKAN COOKIE :')
-token=input('INPUT TOKEN : ')
 open('.cookie.txt','w').write(cookie)
-open('.token.txt','w').write(token)
-os.system('python aerox.py')
+os.system('python aorec.py')
